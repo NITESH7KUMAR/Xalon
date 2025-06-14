6fb3843718c509848af6533551d4dd0f1d8aebba
NITESH KUMAR <119055912+NITESH7KUMAR@users.noreply.github.com>
2025-06-15 00:34:50 +0530

    navbar static  complete

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,9 +1,22 @@
 import react from 'react';
-
+import './navbar.css';
+import logo from '../../assets/logo.png'
 function Navbar(){
   return(
     <>
-    <h1>navbar</h1>
+    <header class="header">
+      <nav class="navbar">
+        <div className='logo'>
+          <img src={logo} alt="X A L O N" />
+        </div>
+      <div className='logo1'>X A L O N</div>
+   <ul class="nav-links">
+    <li class="nav-links1">Login</li>
+    <li class="nav-links1">Register</li>
+    <li class="nav-links1">Admin</li>
+   </ul>
+   </nav>
+   </header>
     </>
   )
 }
