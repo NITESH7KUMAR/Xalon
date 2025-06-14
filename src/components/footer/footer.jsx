8789e8abdbfa4ee5f95f869183ee782f2b00ebe7
NITESH KUMAR <119055912+NITESH7KUMAR@users.noreply.github.com>
2025-06-14 16:30:30 +0530

    basic setup

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,11 @@
+import  React from 'react';
+
+function Footer(){
+  return(
+    <>
+    <h2>Footer</h2>
+    <footer>© 2025 Xalon. All rights reserved.</footer>
+    </>
+  )
+}
+export default Footer;
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,10 @@
+import react from 'react';
+
+function Navbar(){
+  return(
+    <>
+    <h1>navbar</h1>
+    </>
+  )
+}
+export default Navbar;
